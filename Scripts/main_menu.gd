f36b5460392496892a6cd9091172a1bfb3208015
LeYohan-rgb <williamotonielevertz@gmail.com>
2026-09-22 11:30:09 -0400

    Enhanced settings drop panel layout
    Added player settings layout such as customized pfp, lineEdit nodes and a close button node

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -6,8 +6,8 @@ extends Control
 func _ready() -> void:
 	var viewport_size = get_viewport().get_visible_rect().size
 
-	settings_panel.custom_minimum_size.y = viewport_size.y * 0.625
+	settings_panel.custom_minimum_size.y = viewport_size.y * (1200.0 / 1920.0)
 	settings_panel.custom_minimum_size.x = viewport_size.x * (800.0 / 1080.0)
 	
 func _on_settings_button_pressed() -> void:
-	pass # Replace with function body.
+	settings_panel.show()
