5afd65d7e22e82edf3bc0ddf19c8f8a30153185e
LeYohan-rgb <williamotonielevertz@gmail.com>
2026-09-23 12:02:38 -0400

    Added gameplay selection mode scene

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
-@onready var settings_panel = $CenterContainer/settings_panel
+@onready var settings_panel = $Center_setting/settings_panel
+@onready var settings_panel_centered = $Center_setting
 
 
 func _ready() -> void:
@@ -10,4 +11,20 @@ func _ready() -> void:
 	settings_panel.custom_minimum_size.x = viewport_size.x * (800.0 / 1080.0)
 	
 func _on_settings_button_pressed() -> void:
-	settings_panel.show()
+	settings_panel_centered.show()
+
+
+func _on_button_pressed() -> void:
+	settings_panel_centered.hide()
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/gameplay_selection_mode.tscn")
+
+
+func _on_player_1_name_text_changed(new_text: String) -> void:
+	Global.player_name_1 = new_text
+
+
+func _on_player_2_name_text_changed(new_text: String) -> void:
+	Global.player_name_2 = new_text
